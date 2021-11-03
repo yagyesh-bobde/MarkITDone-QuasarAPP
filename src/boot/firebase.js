44a0d086ad043788891c324441384cc045b2b39a
Yagyesh Bobde <yagyeshsrbobde@gmail.com>
2021-11-03 20:36:38 +0530

    logout button

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -17,6 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const auth = getAuth(firebaseApp);
+const firebaseauth = getAuth(firebaseApp);
 
-export default { auth }
+export default { firebaseauth }
